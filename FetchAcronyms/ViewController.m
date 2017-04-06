c13d54b8234ffb6e998d923c9f1125272b4743a8
Mallikarjuna Reddy Palempallinallappagari <mallikarjunareddy@Mallikarjunas-MacBook-Pro.local>
2017-04-06 18:49:26 -0400

    Don't let the user goto next screen if the text is empty

--- FetchAcronyms/ViewController.m
+++ FetchAcronyms/ViewController.m
@@ -22,16 +22,26 @@
 }
 
 -(void)viewWillAppear:(BOOL)animated {
+    //Reset the text field
     self.inputData.text = @"";
 }
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    if ([[segue identifier] isEqualToString:@"show details"])
-    {
-        ResultsTableViewController *rvc = [segue destinationViewController];
-        rvc.currentAcronym = self.inputData.text;
+-(IBAction)fetchDetails:(UIButton *)sender {
+    //Don't go to next screen if the input is empty
+    if (![self.inputData.text isEqualToString:@""]) {
+        [self showDetails];
+    }else{
+        UIAlertController *alertView = [UIAlertController  alertControllerWithTitle:@"Error" message:@"Please enter some text..!!" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) { }];
+        [alertView addAction:okAction];
+        [self presentViewController:alertView animated:YES completion:nil];
     }
+}
+
+- (void)showDetails {
+    ResultsTableViewController *dvc = [[ResultsTableViewController alloc] init];
+    dvc.currentAcronym = self.inputData.text;
+    [self.navigationController pushViewController:dvc animated:YES];
 }
 
 @end
